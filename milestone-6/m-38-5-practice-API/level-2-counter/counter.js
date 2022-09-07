c614fbc846776400c23a41a2339a3dacaa4c1c06
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-09-07 19:08:17 +0300

    level 2 problem updated

--- milestone-6/m-38-5-practice-API/level-2-counter/counter.js
+++ milestone-6/m-38-5-practice-API/level-2-counter/counter.js
@@ -1,12 +1,16 @@
 const btnAdd = document.getElementById('btn-increase');
 let countElem = document.getElementById('counter');
-
-btnAdd.addEventListener('click', ()=>{
-    let countelemvalue = countElem.innerText;
-    let counterValueNumber = parseInt(countelemvalue);
-    countElem.innerText = ++counterValueNumber; 
-    localStorage.setItem('The Count:', countElem.innerText )
-})
+const storedValue = localStorage.getItem('The Count:')
+if(storedValue){
+    btnAdd.addEventListener('click', ()=>{
+        let countelemvalue = countElem.innerText;
+        let counterValueNumber = parseInt(countelemvalue);
+        countElem.innerText = ++counterValueNumber; 
+        localStorage.setItem('The Count:', countElem.innerText )
+    })
+}else{
+    localStorage.setItem('The Count:', 0)
+}
 
 const getItemsFromLocalStorage = () =>{
     const storedValue = localStorage.getItem('The Count:')
