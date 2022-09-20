8ee76945ffcf46bf5846efbf178deeb1f2ac8863
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-09-20 15:19:31 +0300

    done

---
+++ milestone-8/module-45-react-basic/advanced-useeffect-apicall-dynamic-data/src/App.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <Users></Users>
+    </div>
+  );
+}
+
+function Users(){
+  //step-1: declare a state with useState() hook
+
+  const [users, setUsers] = useState([]);
+
+  //step-1: useEffect() hook to fetch data from an api
+  useEffect(()=>{
+    //step-3: Load Data from api inside the useEffect
+    const loadData = async() =>{
+        const res = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await res.json();
+        //step-4: Connect with state(store the data to the initial state declare in useState)
+        setUsers(data) 
+    }
+    loadData();
+  },[]);
+
+  return(
+    <div >
+      {
+        //step-5:  map the data and show the ui
+        users.map(user => <Info name = {user.name}  username = {user.username} email = {user.email}></Info>)
+      }
+    </div>
+  )
+}
+
+
+function Info(props){
+  const {name, username , email} = props;
+  return(
+    <div style = {{border:"2px solid black", width:"50%", marginLeft: "auto", marginRight:"auto", marginTop:"10px"}}>
+      <h1>Name: {name}</h1>
+      <h3>Username: {username}</h3>
+      <h3 >Email: <span style={{color:"blue"}}>{email}</span> </h3>
+    </div>
+  )
+}
+
+export default App;
